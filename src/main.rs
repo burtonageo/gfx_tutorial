e426afa802cf0b935defd5e65cb32b4e18da0416
George Burton <burtonageo@gmail.com>
2017-01-17 01:06:41 +0000

    Implement mouse movement

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-#![feature(duration_checked_ops)]
-
 extern crate angular;
 #[macro_use]
 extern crate gfx;
 extern crate gfx_window_glutin;
 extern crate glutin;
+extern crate num;
 extern crate nalgebra as na;
+extern crate time;
 
 use angular::Angle;
 use gfx::{Device, Factory};
 use gfx::traits::FactoryExt;
 use na::{Isometry3, Perspective3, Point3, Rotation3, ToHomogeneous, Vector3};
-use std::time::{Duration, Instant};
+use num::Zero;
+use time::{Duration, PreciseTime};
+use std::time::Duration as StdDuration;
 
 gfx_defines! {
     vertex Vertex {
@@ -100,22 +102,51 @@ const CUBE: &'static [Vertex] = &[
 
 const CLEAR_COLOR: [f32; 4] = [0.005, 0.005, 0.1, 1.0];
 
+#[derive(Debug)]
+struct Input {
+    position: Point3<f32>,
+    horizontal_angle: Angle<f32>,
+    vertical_angle: Angle<f32>,
+    fov: Angle<f32>
+}
+
+impl Input {
+    fn new() -> Self {
+        Input {
+            position: Point3 { x: 0.0, y: 0.0, z: 10.0 },
+            horizontal_angle: Angle::full(),
+            vertical_angle: Angle::zero(),
+            fov: Angle::eighth()
+        }
+    }
+}
+
+const SPEED: f32 = 19.0;
+const MOUSE_SPEED: f32 = 7.0;
+const DEFAULT_WIN_SIZE: (u32, u32) = (1024, 768);
+
 fn main() {
     let builder = glutin::WindowBuilder::new()
         .with_title("Gfx Example")
-        .with_dimensions(1024, 768)
+        .with_dimensions(DEFAULT_WIN_SIZE.0 as u32, DEFAULT_WIN_SIZE.1 as u32)
         .with_decorations(false)
         .with_vsync();
 
     let (window, mut device, mut factory, main_color, _) =
         gfx_window_glutin::init::<gfx::format::Rgba8, gfx::format::DepthStencil>(builder);
 
+    {
+        window.set_cursor_state(glutin::CursorState::Hide).expect("Could not set cursor state");
+        window.set_cursor_state(glutin::CursorState::Grab).expect("Could not set cursor state");
+        center_cursor_to_window(&window).expect("Could not set cursor position");
+    }
+
+    let mut iput = Input::new();
+
     let mut projection = {
         let aspect = window.get_inner_size_pixels().map(|(w, h)| aspect(w, h)).unwrap_or(1.0f32);
-        Perspective3::new(aspect, Angle::eighth().in_radians(), 0.1, 100.0).to_matrix()
+        Perspective3::new(aspect, iput.fov.in_radians(), 0.1, 100.0)
     };
-    let view = Isometry3::look_at_rh(&Point3::new(4.0f32, 3.0, 3.0), &na::origin(), &Vector3::y())
-        .to_homogeneous();
 
     let mut encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
     let shaders = {
@@ -135,29 +166,86 @@ fn main() {
     };
 
     let mut rot = Rotation3::new(na::zero());
+    let mut last = PreciseTime::now();
     'main: loop {
-        let start = Instant::now();
+        let current = PreciseTime::now();
+        let dt = last.to(current);
+        let dt_s = dt.num_nanoseconds().unwrap_or(0) as f32 / 1_000_000_000.0f32;
+        last = current;
 
-        rot = na::append_rotation(&rot, &Vector3::new(0.0, Angle::Degrees(1.0).in_radians(), 0.0));
-        let model = rot.to_homogeneous();
+        rot = na::append_rotation(&rot, &Vector3::new(0.0, Angle::Degrees(25.0 * dt_s).in_radians(), 0.0));
+
+        let direction = Vector3 {
+            x: iput.vertical_angle.cos() * iput.horizontal_angle.sin(),
+            y: iput.vertical_angle.sin(),
+            z: iput.vertical_angle.cos() * iput.horizontal_angle.cos()
+        };
+
+        let right = Vector3 {
+            x: (iput.horizontal_angle - Angle::quarter()).sin(),
+            y: 0.0,
+            z: (iput.horizontal_angle - Angle::quarter()).cos()
+        };
 
         for e in window.poll_events() {
+            use glutin::{ElementState, Event, MouseScrollDelta, VirtualKeyCode};
             match e {
-                glutin::Event::Closed => break 'main,
-                glutin::Event::Resized(w, h) => {
+                Event::Closed | Event::KeyboardInput(_, _, Some(VirtualKeyCode::Escape)) => break 'main,
+                Event::Resized(w, h) => {
                     // TODO(GAB): This doesn't work on MacOS
-                    projection = Perspective3::new(aspect(w, h), Angle::eighth().in_radians(), 0.1, 100.0)
-                        .to_matrix();
+                    projection.set_aspect(aspect(w, h));
+                }
+                Event::MouseMoved(x, y) => {
+                    let (ww, wh) = window.get_inner_size().unwrap_or(DEFAULT_WIN_SIZE);
+                    let (ww, wh) = (ww as i32, wh as i32);
+
+                    iput.horizontal_angle += Angle::Degrees(MOUSE_SPEED * dt_s * (ww / 2 - x) as f32);
+                    iput.vertical_angle += Angle::Degrees(MOUSE_SPEED * dt_s * (wh / 2 - y ) as f32);
+
+                    iput.horizontal_angle = iput.horizontal_angle.normalized();
+                    iput.vertical_angle = iput.vertical_angle.normalized();
+
+                    center_cursor_to_window(&window).expect("Could not set cursor position");
+                }
+                Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::Up)) => {
+                    iput.position -= direction * SPEED * dt_s;
+                }
+                Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::Down)) => {
+                    iput.position += direction * SPEED * dt_s;
+                }
+                Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::Left)) => {
+                    iput.position += right * SPEED * dt_s;
+                }
+                Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::Right)) => {
+                    iput.position -= right * SPEED * dt_s;
+                }
+                Event::MouseWheel(delta, _) => {
+                    let dy = match delta {
+                        MouseScrollDelta::LineDelta(_, y) => y,
+                        MouseScrollDelta::PixelDelta(_, y) => y
+                    };
+                    iput.fov = (iput.fov + Angle::Degrees(dy / 5.0)).normalized();
+                    projection.set_fovy(iput.fov.in_radians());
+                }
+                Event::Focused(_gained) => {
                 }
                 _ => (),
             }
         }
 
+        let view = {
+            let up = na::cross(&right, &direction);
+            Isometry3::look_at_lh(&iput.position,
+                                  &(iput.position.to_vector() + direction).to_point(),
+                                  &up)
+        };
+
         encoder.clear(&data.out, CLEAR_COLOR);
 
+        let mvp = projection.to_matrix() * view.to_homogeneous() * rot.to_homogeneous();
         encoder.update_constant_buffer(&data.locals,
                                        &Locals {
-                                           transform: *(projection * view * model).as_ref()
+                                           transform: *(mvp).as_ref()
                                        });
 
         encoder.draw(&slice, &pso, &data);
@@ -165,9 +253,18 @@ fn main() {
         window.swap_buffers().unwrap();
         device.cleanup();
 
-        let dt = Instant::now().duration_since(start);
-        std::thread::sleep(Duration::from_millis(16).checked_sub(dt).unwrap_or(Duration::new(0, 0)));
+        let sleep_time = Duration::milliseconds(12)
+            .checked_sub(&dt)
+            .unwrap_or(Duration::zero())
+            .to_std()
+            .unwrap_or(StdDuration::from_millis(0));
+        std::thread::sleep(sleep_time);
     }
+}
+
+fn center_cursor_to_window(window: &glutin::Window) -> Result<(), ()> {
+    let (ww, wh) = window.get_inner_size().unwrap_or(DEFAULT_WIN_SIZE);
+    window.set_cursor_position(ww as i32 / 2, wh as i32 / 2)
 }
 
 fn aspect(w: u32, h: u32) -> f32 {
